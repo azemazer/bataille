6cb42ccda9ed70bbec28db39097662b4ff7d7a01
azemazer <a.laval16@laposte.net>
2023-07-30 16:21:19 +0200

    duel funcitonal, but both lists empty themselves

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ for i in range(len(fullGame)):
 player1Game = []
 player2Game = []
 
+player1Cards = []
+player2Cards = []
+
 gameState = 'isOver'
+duelState = 'isOver'
 
 # General game purpose functions
 
@@ -38,12 +42,21 @@ def cardValor(card):
         else:
              print('Card value error')
     return cardValue
+
+def drag(gameOfCards, numberOfCards):
+    currentCards = []
+    currentGameOfCards = gameOfCards
+    for i in range (numberOfCards):
+        currentCards.append(gameOfCards[-1])
+        currentGameOfCards.remove(gameOfCards[-1])
+    return currentCards
+          
             
 
 
 # State functions 
 
-def doIsOver():
+def doGameIsOver():
     global gameState
     global player1Game
     global player2Game
@@ -51,8 +64,10 @@ def doIsOver():
     newGame = input('New game? Y/N')
     if (newGame == 'Y' or newGame == 'y'):
         print('Game On!')
-        player1Game = random.shuffle(fullGame)
-        player2game = random.shuffle(fullGame)
+        player1Game = fullGame
+        random.shuffle(player1Game)
+        player2Game = fullGame
+        random.shuffle(player2Game)
         gameState = 'isOn'
     elif (newGame == 'N' or newGame == 'n'):
         print('Alright, see you!')
@@ -60,21 +75,59 @@ def doIsOver():
     else:
         print('Wrong key! Try again.')
 
-def doIsOn():
+def doGameIsOn():
     global gameState
+    global duelState
     global player1Game
     global player2Game
 
-    player1Cards = drag(player1Game, 1)
-    player2Cards = drag(player2Game, 1)
+    global player1Cards
+    global player2Cards
 
-    print('test')
-    gameState = 'isOver'
+    if duelState == 'isOver':
+        if not player1Game:
+            print("player 2 won!")
+            gameState = 'isOver'
+        elif not player2Game:
+            print("player 1 won!")
+            gameState = 'isOver'
+        else:
+            random.shuffle(player1Game)
+            random.shuffle(player2Game)
+            player1Cards = drag(player1Game, 1)
+            player2Cards = drag(player2Game, 1)
+            duelState = 'isOn'
+
+    elif duelState == 'isOn':
+
+        if cardValor(player1Cards[-1]) > cardValor(player2Cards[-1]):
+            player1Game += player2Cards
+            print("Player 1 won the cards: ")
+            for i in range (len(player2Cards)):
+                print (player2Cards[i])
+                player2Game.remove(player2Cards[i])
+                print ("Player 2 has " + str(len(player2Game)) + " cards left")
+            duelState = 'isOver'
+
+        elif cardValor(player2Cards[-1]) > cardValor(player1Cards[-1]):
+            player2Game += player1Cards
+            print("Player 2 won the cards: ")
+            for i in range (len(player1Cards)):
+                print (player1Cards[i])
+                player1Game.remove(player1Cards[i])
+                print ("Player 1 has " + str(len(player1Game)) + " cards left")
+            duelState = 'isOver'
+
+        elif cardValor(player2Cards[-1]) == cardValor(player1Cards[-1]):
+            print("Bataille between two " + player2Cards[-1] + " !")
+            player1Cards += drag(player1Game, 2)
+            player2Cards += drag(player2Game, 2)
+         
 
 
 # State loop
 while True:
     if gameState =='isOver':
-        doIsOver()
+        doGameIsOver()
     elif gameState == 'isOn':
-        doIsOn()
+        doGameIsOn()
